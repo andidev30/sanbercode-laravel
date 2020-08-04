11a009f481d42f69dd045f2892ab20b795890d6f
Andidev30 <aandi30082001@gmail.com>
2020-08-04 16:10:19 +0700

    add routes /

--- Pekan3/Day2/routes/web.php
+++ Pekan3/Day2/routes/web.php
@@ -13,11 +13,13 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+Route::get('/', 'HomeController@spa');
 
 Route::get('/register', 'AuthController@index');
 
-// Route::get('/welcome', 'HomeController@index');
+Route::get('/welcome', 'HomeController@spa');
 Route::post('/welcome', 'HomeController@index');
